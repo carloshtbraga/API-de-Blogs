ba24bb67ee7582d7dd3e8afe22c6e29dfa236c29
carloshtbraga <carloshtbraga@gmail.com>
2023-05-13 12:21:34 -0300

    bugs

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -4,8 +4,7 @@ const { tokenGenerator } = require('../utils/tokenTable');
 const login = async (req, res) => {
   const { email } = req.body;
   const dataValues = await loginService.checkIfUserExists(email);
-  if (dataValues === undefined
-    ) {
+  if (dataValues.length === 0) {
     return res.status(400).json({ message: 'Invalid fields' });
   }
   return res.status(200).json({ token: tokenGenerator(email) });
